62ef3bbc5d1997e50e110a4582c874c53f64d86d
rmst <rmst@qq.com>
2024-08-18 14:58:54 +0800

    htmlDist

--- src/main/htmlServer.ts
+++ src/main/htmlServer.ts
@@ -1,5 +1,5 @@
 import liveServer from 'live-server'
 import path from 'path'
 
-const params = { port: 8083, root: path.join(__dirname, '../../resources/dist'), open: false }
+const params = { port: 8083, root: path.join(__dirname, '../../resources/htmlDist'), open: false }
 liveServer.start(params)
